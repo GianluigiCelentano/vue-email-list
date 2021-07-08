56cba5f632a3143be15b4bd2ed64a4abac284dd9
GianluigiCelentano <cgianluigi96@gmail.com>
2021-07-08 16:32:08 +0200

    vue email list fine

---
+++ script.js
@@ -0,0 +1,21 @@
+Vue.config.devtools = true
+
+const app = new Vue ({
+    el: '#app',
+    data: {
+        emails: []
+    }, 
+    created() {
+        this.getEmails
+    },
+    methods: {
+        getEmails: function() {
+            for (var i = 0; i < 10; i++) {
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((response) => {
+            this.emails.push(response.data.response)
+                })
+            }
+        }
+    }
+})
